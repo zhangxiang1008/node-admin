6b03a2776625e000199e4aa41f24d88115d85a31
cddezx <zxwfly08@163.com>
2023-11-17 23:47:53 +0800

    -update 静态文件服务中间件

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
+const fs = require('fs');
+
+const evil = require('./config/evil');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -8,13 +12,17 @@ const app = express();
 
 // 1) MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+  app.use(morgan('short'));
 }
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
+  // 黑名单
+  if (evil.EvilIps.includes(req.ip)) {
+    res.status(401).send('not allowed');
+  }
   console.log('Hello from the middleware 👋');
   next();
 });
@@ -23,7 +31,7 @@ app.use((req, res, next) => {
     '🚀-----time',
     `${new Date().getFullYear()}-${new Date().getMonth()}-${new Date().getDate()} 请求url:${
       req.url
-    }`
+    } id=${req.ip}`
   );
   next();
 });
@@ -31,6 +39,17 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
+});
+
+app.use(function(req, res, next) {
+  const filePath = path.join(__dirname, 'static', req.url);
+  fs.stat(filePath, function(err, exists) {
+    if (exists) {
+      res.sendFile(filePath);
+    } else {
+      next();
+    }
+  });
 });
 
 // 3) ROUTES
---
+++ config/evil.js
@@ -0,0 +1,5 @@
+// const EvilIps = ['190.193.10.123', '192.168.1.3', '::ffff:127.0.0.1'];
+const EvilIps = ['190.193.10.123', '192.168.1.3'];
+module.exports = {
+  EvilIps
+};
